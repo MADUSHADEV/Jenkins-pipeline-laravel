156b000c3b3d6cfc911bd54b13c66e998a1c98c2
Madusha Pravinda <madusha.dev001@gmail.com>
2025-10-16 13:12:48 +0530

    Update sample user phone number in controller
    Changed the hardcoded phone number in HelloWorldController from '123-456-7890' to '123-456-4570' for sample user data.

--- app/Http/Controllers/HelloWorldController.php
+++ app/Http/Controllers/HelloWorldController.php
@@ -21,7 +21,7 @@ class HelloWorldController extends Controller
             'id' => $id,
             'name' => 'Sample User ' . $id,
             'email' => 'user' . $id . '@example.com',
-            'phone' => '123-456-7890',
+            'phone' => '123-456-4570',
             'message' => 'This is a sample message for user ' . $id,
             'created_at' => now()->subDays(rand(1, 30))
         ];
