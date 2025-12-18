06696b29f56f084213af918669b2d8fd426c3114
Madusha Pravinda <madusha.dev001@gmail.com>
2025-12-18 12:45:04 +0530

    Remove spaces around require statements in web.php
    Updated the require statements for settings.php and auth.php to remove spaces around the concatenation operator for consistency with code style.

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('webhook-test');
 });
 
-require __DIR__ . '/settings.php';
-require __DIR__ . '/auth.php';
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
